13bfcd0dbaf029a8244f700cf17fa9b9090f0d59
Jott <julian.meder1999@web.de>
2024-11-18 18:44:02 +0100

    implemented delete_from_inventory in main.py, added save_changes_to_excel_file function, correct_inventory_item function and create_data_frame function

--- main.py
+++ main.py
@@ -1,54 +1,53 @@
-import os
-from operator import index
-
 import pandas as pd
-from manage_inventory_funcs import add_to_inventory, delete_from_inventory
+from manage_inventory_funcs import add_to_inventory, delete_from_inventory, save_changes_to_excel_file, \
+    create_data_frame
 
+if __name__ == "__main__":
 
-file_path = "C:/Users/julia/Documents/dnd_programme/dnd_inventory.xlsx"
-excel_frame = pd.read_excel(file_path)
-print(excel_frame)
-inventory = dict(zip(excel_frame["Items"],excel_frame["Count"]))
-excel_frame.to_excel(file_path, index=False, sheet_name="Inventory")
-print(inventory)
-
-
-
-running = True
-while running:
-    player_input = input("Welche Aktion willst du ausführen?: ").lower()
-
-    if player_input == "help":
-        print("""
-            Du kannst folgende Commands ausführen:
-            end - Programm beenden
-            display - aktuelles Inventar ausgeben
-            add - item zu Inventar hinzufügen
-            remove - item aus Inventar entfernen
-            remove last - aktuellstes Item entfernen
-            change -  Schreibfehler korrigieren
-            currency - Geldwerte verwalten
-              """)
-
-    elif player_input == "end":
-        running = False
-
-    elif player_input == "display":
-        print("\n", excel_frame, "\n")
-
-    elif player_input == "add":
-        add_to_inventory(inventory)
-        excel_frame = pd.DataFrame(list(inventory.items()), columns=["Items", "Count"])
-        with pd.ExcelWriter(file_path, mode="a", if_sheet_exists="replace") as writer:
-            excel_frame.to_excel(writer, index=False, sheet_name="Inventory")
-        #als funktion zusammenfassen!!
+    file_path = "C:/Users/julia/Documents/dnd_programme/dnd_inventory.xlsx"
+    excel_frame = pd.read_excel(file_path)
+    print(excel_frame)
+    inventory = dict(zip(excel_frame["Items"],excel_frame["Count"]))
+    print(inventory)
 
 
 
-    else:
-        print("Bitte gebe ein bekanntes Command ein. Diese kannst du mit help nachschlagen.")
+    running = True
+    while running:
+        player_input = input("Welche Aktion willst du ausführen?: ").lower()
+
+        if player_input ==  "help":
+            print("""
+                Du kannst folgende Commands ausführen:
+                end - Programm beenden
+                display - aktuelles Inventar ausgeben
+                add - item zu Inventar hinzufügen
+                remove - item aus Inventar entfernen
+                change -  Schreibfehler korrigieren
+                currency - Geldwerte verwalten
+                  """)
+
+        elif player_input == "end":
+            running = False
+
+        elif player_input == "display":
+            print("\n", create_data_frame(inventory), "\n")
+
+        elif player_input == "add":
+            inventory = add_to_inventory(inventory)
+            save_changes_to_excel_file(inventory, file_path)
+
+
+        elif player_input == "remove":
+            inventory = delete_from_inventory(inventory)
+            save_changes_to_excel_file(inventory, file_path)
+
+        else:
+            print("Bitte gebe ein bekanntes Command ein. Diese kannst du mit help nachschlagen.")
 
 
 
 
-commands = ["add", "remove", "remove last", "currency", "display inventory","end", "help"]
+    commands = ["add", "remove", "remove last", "currency", "display inventory","end", "help"]
+
+    # mehr mit dataframe und pandas in testground ausprobieren
--- manage_inventory_funcs.py
+++ manage_inventory_funcs.py
@@ -1,16 +1,14 @@
-
+import pandas as pd
 
 def add_to_inventory(inventory):
     item = input("Welches Item willst du deinem Inventar hinzufügen?: ").lower()
-    count = int(input(f"Anzahl der {item}: "))
+    count = int(input(f"Anzahl an {item}: "))
     if item not in inventory:
         inventory.setdefault(item, count)
     else:
         new_count = inventory.get(item) + count
         inventory.update({item: new_count})
-
-
-
+    return inventory
 
 def delete_from_inventory(inventory):
     item = input("Welches Item soll aus dem Inventar genommen weden?: ").lower()
@@ -20,15 +18,17 @@ def delete_from_inventory(inventory):
         inventory.update({item: new_count})
     else:
         inventory.pop(item)
+    return inventory
 
+def correct_inventory_item(inventory):
+    item_old = input("Welches Item soll korrigiert werden?: ")
+    item_new = input("Wie heißt das neue Item?: ")
+    inventory[item_new] = inventory.pop(item_old)
 
-backpack = {}
-if __name__ == "__main__":
-    add_to_inventory(backpack)
-    print(backpack)
-    delete_from_inventory(backpack)
-    print(backpack)
-    add_to_inventory(backpack)
-    print(backpack)
-    add_to_inventory(backpack)
-    print(backpack)
+def save_changes_to_excel_file(dict_inventory:dict , data_path):
+    df_inventory = create_data_frame(dict_inventory)
+    with pd.ExcelWriter(data_path, mode="a", if_sheet_exists="replace") as writer:
+        df_inventory.to_excel(writer, index=False, sheet_name="Inventory")
+
+def create_data_frame(dict_inventory):
+    return pd.DataFrame({"Items": dict_inventory.keys(), "Count": dict_inventory.values()})
