9d600f0741abbfb64c5695244cac6a4b9f05ff2b
Jott <julian.meder1999@web.de>
2024-12-01 15:52:02 +0100

    added more ut in manage_wallet_funcs_ut.py

--- manage_wallet_funcs_ut.py
+++ manage_wallet_funcs_ut.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 from manage_wallet_funcs import manage_currencies
 import unittest
@@ -8,12 +9,37 @@ data = {"Currency": ["CP","SP","GP","PP"],
 data_frame = pd.DataFrame(data)
 
 
+
 class TestManageCurrencies(unittest.TestCase):
     @patch("builtins.input", side_effect=["CP", "+10", "0"])
-    @patch("builtins.print")
-    def test_add_to_valid_currency(self, mock_input, mock_print):
+    def test_add_to_valid_currency(self, mock_input):
         manage_currencies(data_frame)
         updated_cp_value = data_frame.loc[data_frame["Currency"] == "CP", "Amount"].iloc[0]
         self.assertEqual(updated_cp_value, 20)
 
-    @patch()
+    @patch("builtins.input", side_effect=["SP", "-10", "0"])
+    def test_remove_from_valid_currency(self, mock_input):
+        manage_currencies(data_frame)
+        updated_sp_value = data_frame.loc[data_frame["Currency"] == "SP", "Amount"].iloc[0]
+        self.assertEqual(updated_sp_value, 0)
+
+    @patch("builtins.input", side_effect=["gp", "0", "0"])
+    def test_lowercase_currency(self, mock_input):
+        manage_currencies(data_frame)
+        gp_value = data_frame.loc[data_frame["Currency"] == "GP", "Amount"].iloc[0]
+        self.assertEqual(gp_value, 10)
+
+    @patch("builtins.input", side_effect=["PP", "-2","PP","-3", "PP", "-5", "0"])
+    def test_remove_multiple_times_from_valid_currency(self, mock_input):
+        manage_currencies(data_frame)
+        updated_pp_value = data_frame.loc[data_frame["Currency"] == "PP", "Amount"].iloc[0]
+        self.assertEqual(updated_pp_value, 0)
+
+    @patch("builtins.input", side_effect=["EE", "SP", "+8", "0"])
+    @patch("builtins.print")
+    def test_wrong_currency_name(self, mock_input, mock_print):
+        manage_currencies(data_frame)
+        updated_sp_value = data_frame.loc[data_frame["Currency"] == "SP", "Amount"].iloc[0]
+        self.assertEqual(updated_sp_value, 8)
+
+#data_frame nach jedem unittest clearen und mock_print recherchieren um expected prints mit actual prints zu vergleichen
